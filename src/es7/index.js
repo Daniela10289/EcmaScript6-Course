e3373d1f8533dd6236fbf97eda62fa4e8e1d0be5
Daniela10289 <dpalacio2200@gmail.com>
2021-07-28 14:20:12 -0500

    Analisis de EcmaScript 7

---
+++ src/es7/index.js
@@ -0,0 +1,15 @@
+let numbers = [1,2,3,7,8];
+
+// Incluide: Valida si se encuentra el valor 7
+if (numbers.includes(9)) {
+    console.log('Se encuentra el 7');
+} else {
+    console.log('No se encuentra el numero')
+}
+
+ let base = 4;
+ let exponent = 4;
+ // calcula base a la potencia(exponente)
+ let result = base ** exponent;
+
+ console.log(result);
